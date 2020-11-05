f72f22ff0ee044a8662b9d0995b1bf17cc2833e6
Nahid <Nahid@mislbd.com>
2020-11-05 17:13:08 +0600

    Added Basic File

---
+++ ASPBasicProjectWithAuth/ASPBasicProjectWithAuth/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPBasicProjectWithAuth.Models;
+using ASPBasicProjectWithAuth.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPBasicProjectWithAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_employeeRepository.GetAllEmployees());
+        }
+        public IActionResult Details()
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Details Employee"
+            };
+           
+            return View(homeDetailsViewModel);
+        }
+    }
+}
---
+++ ASPBasicProjectWithAuth/ASPBasicProjectWithAuth/Models/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPBasicProjectWithAuth.Models
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployee(int Id);
+    }
+}
